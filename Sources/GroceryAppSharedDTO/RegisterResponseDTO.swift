0b439b858d73c38d1342f407249aaf56edff16e8
Luka Zlatarić <luka.zlataric2402@gmail.com>
2025-09-11 20:14:13 +0200

    Creating base models

---
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
